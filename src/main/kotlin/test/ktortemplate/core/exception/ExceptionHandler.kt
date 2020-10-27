7083cf9ce60b04ef2c3ee2827812508bd4c84abf
Pedro Henriques <pedro.henriques@waterdog.mobi>
2020-10-27 13:56:33 +0000

    Returning request id from ErrorDTO

--- src/main/kotlin/test/ktortemplate/core/exception/ExceptionHandler.kt
+++ src/main/kotlin/test/ktortemplate/core/exception/ExceptionHandler.kt
@@ -4,27 +4,23 @@ import io.ktor.application.call
 import io.ktor.features.StatusPages
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.MDC
 
 fun StatusPages.Configuration.appException() {
+    val log = LoggerFactory.getLogger(StatusPages.Configuration::class.java)
 
     exception<AppException> {
-        val requestId = MDC.get("X-Request-Id")
-        val errorData = ErrorDTO(it.code.httpStatusCode.value, it.code.messageCode, it.title, requestId, it.errors)
-        log.error("Returning AppException data to client (X-Request-Id=$requestId):\n$errorData")
+        val errorData = ErrorDTO(it.code.httpStatusCode.value, it.code.messageCode, it.title, it.errors)
+        log.error("Returning AppException data to client:\n$errorData", it)
         call.respond(it.code.httpStatusCode, errorData)
     }
 
     exception<Throwable> {
-        val requestId = MDC.get("X-Request-Id")
-        log.error("Returning exception (X-Request-Id=$requestId)")
+        log.error("Returning Throwable", it)
         call.respond(
             ErrorDTO(
                 httpStatusCode = HttpStatusCode.InternalServerError.value,
-                title = it.localizedMessage,
-                requestId = requestId
+                title = it.localizedMessage
             )
         )
     }
@@ -32,13 +28,9 @@ fun StatusPages.Configuration.appException() {
 
 fun StatusPages.Configuration.defaultStatusCodes() {
     status(HttpStatusCode.NotFound) {
-        val requestId = MDC.get("X-Request-Id")
-        call.respond(
-            HttpStatusCode.NotFound,
-            ErrorDTO(httpStatusCode = HttpStatusCode.NotFound.value, requestId = requestId)
-        )
+        call.respond(HttpStatusCode.NotFound, ErrorDTO(httpStatusCode = HttpStatusCode.NotFound.value))
     }
     // TODO customize response for some status codes if necessary
 }
 
-val log: Logger get() = LoggerFactory.getLogger(StatusPages.Configuration::class.java)
+
